a01dc2278a0544e799c862bdb86e1ab0eb848ed2
Elbasri Abdennacer <devnasser@gmail.com>
2023-07-31 21:36:10 +0100

    Update CartWithImages.php

--- Cartwithimagesapi/Model/CartWithImages.php
+++ Cartwithimagesapi/Model/CartWithImages.php
@@ -4,8 +4,9 @@ namespace Elbasri\Cartwithimagesapi\Model;
 
 use Elbasri\Cartwithimagesapi\Api\CartWithImagesInterface;
 use Magento\Quote\Model\QuoteRepository;
-use Psr\Log\LoggerInterface;
+//use Psr\Log\LoggerInterface;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class CartWithImages implements CartWithImagesInterface
 {
@@ -17,16 +18,23 @@ class CartWithImages implements CartWithImagesInterface
     /**
      * @var \Psr\Log\LoggerInterface
      */
-    protected $logger;
+    //protected $logger;
+
+    /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    protected $productRepository;
 
     public function __construct(
         QuoteRepository $quoteRepository,
-        LoggerInterface $logger, // Add this line to inject the logger
-        StoreManagerInterface $storeManager
+        //LoggerInterface $logger, // Add this line to inject the logger
+        StoreManagerInterface $storeManager,
+        ProductRepositoryInterface $productRepository
     ) {
         $this->quoteRepository = $quoteRepository;
-        $this->logger = $logger; // Assign the injected logger to the class property
+        //$this->logger = $logger; // Assign the injected logger to the class property
         $this->storeManager = $storeManager; 
+        $this->productRepository = $productRepository;
     }
     /**
      * @inheritDoc
@@ -35,45 +43,51 @@ class CartWithImages implements CartWithImagesInterface
     {
         $cart = $this->quoteRepository->getActiveForCustomer($customerId);
 
-        $baseImageUrl = $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA);
-
         $cartItems = [];
         foreach ($cart->getAllVisibleItems() as $item) {
             $product = $item->getProduct();
-            $productImage = $product->getMediaGalleryEntries();
-            $productImageUrl = null;
+            $productImage = $this->getProductImage($product->getId());
 
-            if ($productImage) {
-                foreach ($productImage as $image) {
-                    if ($image['media_type'] === 'image') {
-                        $productImageUrl = $baseImageUrl . 'catalog/product' . $image['file'];
-                        break;
-                    }
-                }
-            } else {
-                // Use a default image URL when no product images are available
-                $productImageUrl = $baseImageUrl . 'placeholder/default.jpg';
-            }
-
-            // Log information to debug
-            $this->logger->debug('Product ID: ' . $product->getId());
-            $this->logger->debug('Product Name: ' . $product->getName());
-            $this->logger->debug('Product Image: ' . $productImageUrl);
-
-            // Add the necessary cart item data along with the full product image URL
+            // Add the necessary cart item data along with the product image
             $cartItems[] = [
                 'item_id' => $item->getId(),
                 'sku' => $item->getSku(),
                 'name' => $item->getName(),
                 'price' => $item->getPrice(),
                 'quantity' => $item->getQty(),
-                'product_image' => $productImageUrl,
+                'product_image' => $productImage,
                 'product_type' => $product->getTypeId(),
                 'quote_id' => $cart->getId()
             ];
         }
 
         return $cartItems;
+    }
+
+    /**
+     * Get product image URL
+     *
+     * @param int $productId
+     * @return string|null
+     */
+    private function getProductImage($productId)
+    {
+        try {
+            // Use ProductRepositoryInterface to get the product with media gallery entries
+            $product = $this->productRepository->getById($productId);
+            $mediaGalleryEntries = $product->getMediaGalleryEntries();
+
+            // Check if there are media gallery entries for the product
+            if (!empty($mediaGalleryEntries)) {
+                // Get the first media gallery entry URL
+                $productImage = reset($mediaGalleryEntries)->getFile();
+                return $productImage;
+            }
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        return null;
     }
 
 }
