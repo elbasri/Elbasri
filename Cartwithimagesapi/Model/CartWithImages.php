3e86078b3b148725335fa94003acf9697e273a68
Elbasri Abdennacer <devnasser@gmail.com>
2023-07-31 18:54:46 +0100

    Update CartWithImages.php

--- Cartwithimagesapi/Model/CartWithImages.php
+++ Cartwithimagesapi/Model/CartWithImages.php
@@ -4,6 +4,7 @@ namespace Elbasri\Cartwithimagesapi\Model;
 
 use Elbasri\Cartwithimagesapi\Api\CartWithImagesInterface;
 use Magento\Quote\Model\QuoteRepository;
+use Magento\Store\Model\StoreManagerInterface;
 
 class CartWithImages implements CartWithImagesInterface
 {
@@ -12,10 +13,17 @@ class CartWithImages implements CartWithImagesInterface
      */
     protected $quoteRepository;
 
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
     public function __construct(
-        QuoteRepository $quoteRepository
+        QuoteRepository $quoteRepository,
+        StoreManagerInterface $storeManager
     ) {
         $this->quoteRepository = $quoteRepository;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -28,7 +36,9 @@ class CartWithImages implements CartWithImagesInterface
         $cartItems = [];
         foreach ($cart->getAllVisibleItems() as $item) {
             $product = $item->getProduct();
-            $productImage = $product->getImage();
+            $productImage = $this->storeManager
+                ->getStore()
+                ->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $product->getImage();
             
             // Add the necessary cart item data along with the product image
             $cartItems[] = [
